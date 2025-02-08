2a3f30c0405881b620f5f01e7e4cbaed7b1339df
Kristaps Zaķis <kristaps.zakis@gmail.com>
2025-02-08 09:58:17 +0200

    started to implement search section

---
+++ app/components/tasks-area/search-input.tsx
@@ -0,0 +1,7 @@
+import { Input } from '@/components/ui/input';
+
+export default function SearchInput() {
+  return (
+    <Input type="text" placeholder="Filter By Tasks..." className="h-10" />
+  );
+}
---
+++ app/components/tasks-area/tasks-area.tsx
@@ -0,0 +1,19 @@
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import SearchInput from './search-input';
+import { IoCloseSharp } from 'react-icons/io5';
+
+export default function TasksArea() {
+  return (
+    <Card>
+      <CardHeader>
+        <div className=""></div>
+      </CardHeader>
+    </Card>
+  );
+}
